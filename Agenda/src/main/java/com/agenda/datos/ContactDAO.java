990207aeea19456cebc345412278677d688c05c8
albertop10 <albertop@hotmail.es>
2018-06-01 11:43:29 +0200

    datos

--- Agenda/src/main/java/com/agenda/datos/ContactDAO.java
+++ Agenda/src/main/java/com/agenda/datos/ContactDAO.java
@@ -1,5 +1,64 @@
 package com.agenda.datos;
 
-public class ContactDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.cfg.Configuration;
+
+public class ContactDAO {
+		
+public static List<Persona> devolverPersona(String sql) {
+		
+		List<Persona> listPerson = null;
+		try {
+			ResultSet result = createStatement().executeQuery(sql);
+
+			listPerson = new ArrayList<Persona>();
+			result.beforeFirst();
+			while (result.next()) {
+
+				
+				listPerson.add(new Persona(result.getString(2),result.getString(3)));
+				System.out.println("Entra en result");
+
+			}
+		} catch (Exception e) {
+			System.err.println("--- ERROR: Problem in ejecutaQuery");
+			System.err.println(e.getMessage());
+		}
+		if(listPerson == null)
+			System.out.println("Vacia");
+		
+		System.out.println("mi sql"+sql);
+		return listPerson;
+	}
+
+	private static Statement createStatement() {
+		Connection conn = null;
+		Statement st = null;
+
+		Configuration cfg = new Configuration();
+		/* Parametros para la configuracion extraidos del cfg.xml */
+		String dbdriver = cfg.configure().getProperty("hibernate.connection.driver_class");
+		String url = cfg.getProperty("hibernate.connection.url");
+		String username = cfg.getProperty("hibernate.connection.username");
+		String password = cfg.getProperty("hibernate.connection.password");
+
+		try {
+			Class.forName(dbdriver);
+			conn = DriverManager.getConnection(url, username, password);
+			st = conn.createStatement();
+
+		} catch (Exception e) {
+			System.err.println("--- ERROR: Hay una excepcion");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return st;
+	}
+	
 }
